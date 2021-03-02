058febc17a4de534caa4ec5996099cb8e8971818
Arthur Dayton <arthur@arthurdayton.com>
2021-03-01 23:17:35 -0800

    ec2 post - continued

--- src/blogs/3/images.js
+++ src/blogs/3/images.js
@@ -3,9 +3,15 @@ import ARCH_EC2_DRAWING from './EC2_Components.png'
 import ROLE_POLICY from './roleWithPolicy.png'
 import POLICY_ROLE from './policyWithRole.png'
 import TRUST_RELATIONSHIP from './trustRelationship.png'
+import WEBSERVER from './ec2webserver.png'
+import TF_FOLDER from './terraform_ec2.png'
+import SERVICEFILE from './ServiceFile.png'
 
 export const ARCH_DRAWING_IMAGE = ARCH_DRAWING;
 export const ARCH_EC2_DRAWING_IMAGE = ARCH_EC2_DRAWING;
 export const ROLE_POLICY_IMAGE = ROLE_POLICY;
 export const POLICY_ROLE_IMAGE = POLICY_ROLE;
 export const TRUST_RELATIONSHIP_IMAGE = TRUST_RELATIONSHIP;
+export const WEBSERVER_IMAGE = WEBSERVER;
+export const TF_FOLDER_IMAGE = TF_FOLDER;
+export const SERVICEFILE_IMAGE = SERVICEFILE;
